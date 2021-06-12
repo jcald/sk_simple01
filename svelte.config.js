94403ae6b0723776c75d2404f4d6a10016e3383d
José Calderón Celis <josecalderoncelis@gmail.com>
2021-06-12 01:04:20 -0500

    Inicio

---
+++ svelte.config.js
@@ -0,0 +1,11 @@
+import netlify from '@sveltejs/adapter-netlify';
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: netlify(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
